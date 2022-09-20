090bf3a082f5e9e6b8fdf37e28c52408e59f781b
Mahdi <mahdipakravan79@gmail.com>
2022-09-20 09:22:53 +0430

    feat : init hook form

--- components/modal/register/register.modal.tsx
+++ components/modal/register/register.modal.tsx
@@ -3,18 +3,24 @@ import {PopupContext} from "../../../libs/context/popup.context";
 import {Modal} from "../modal";
 import {Input} from "../../ui/input";
 import {Button} from "../../ui/button";
+import {useForm} from "react-hook-form";
 
 type Props = {
 
 };
 export const RegisterModal = (props: Props) => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const {closeAllPopup} = useContext(PopupContext);
 
     return (
         <Modal onBackClick={closeAllPopup} popupTitle={"Register Popup"} desc={"register to this most good application"}>
-            <Input placeholder={"Email address : "} type={"email"}/>
-            <Input placeholder={"Password"} type={"password"} className={"mt-4"}/>
-            <Button type={"submit"} className={"mt-4"}>Login</Button>
+            <form onSubmit={handleSubmit((value) => {
+                alert(JSON.stringify(value))
+            })}>
+                <Input placeholder={"Email address : "} type={"email"} register={register("email")}/>
+                <Input placeholder={"Password"} type={"password"} className={"mt-4"} register={register("password")}/>
+                <Button type={"submit"} className={"mt-4"}>Login</Button>
+            </form>
         </Modal>
     );
 };
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,11 +1,12 @@
 import {AllHTMLAttributes} from "react";
 import clsx from "clsx";
+import {UseFormRegister} from "react-hook-form";
 
 type Props = {
-
+    register : ReturnType<UseFormRegister<any>>
 } & AllHTMLAttributes<HTMLInputElement>;
 export const Input = (props: Props) => {
     return (
-        <input {...props} className={clsx("form-input px-4 py-2 rounded w-full bg-background border-none" , props.className)}/>
+        <input {...props} {...props.register} className={clsx("form-input px-4 py-2 rounded w-full bg-background border-none" , props.className)}/>
     );
 };
